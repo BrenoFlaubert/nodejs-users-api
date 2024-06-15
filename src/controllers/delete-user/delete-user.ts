33586e6ea5b131819d3c4223bcbbc86ad9f46a48
Breno Flaubert de Souza <brenoflaubert@gmail.com>
2024-06-15 19:18:19 -0300

    feat: delete user controller with route

--- src/controllers/delete-user/delete-user.ts
+++ src/controllers/delete-user/delete-user.ts
@@ -1,5 +1,29 @@
-import { IDeleteUserController } from "./protocols";
+import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "../protrocols";
+import { IDeleteUserController, IDeleteUserRepository } from "./protocols";
 
 export class DeleteUserController implements IDeleteUserController {
+    constructor(private readonly deleteUserRepository: IDeleteUserRepository) {}
+    async handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>> {
+        try {
+            const id = httpRequest?.params?.id
+            if(!id){
+                return {
+                    statusCode: 400,
+                    body: 'Missing user Id'
+                }
+            }
+            const user = await this.deleteUserRepository.deleteUser(id)
+            return {
+                statusCode: 200,
+                body: user
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: "Something went wrong"
+            }
+        }
+    }
 
 }
--- src/controllers/delete-user/protocols.ts
+++ src/controllers/delete-user/protocols.ts
@@ -1,6 +1,8 @@
 import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "../protrocols";
 
 export interface IDeleteUserController {
+    handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>> 
 }
 
 export interface IDeleteUserRepository {
