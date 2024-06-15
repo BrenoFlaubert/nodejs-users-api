325cf26e0dc7ffac32fb2e4cb2b88714b94e829c
Breno Flaubert de Souza <brenoflaubert@gmail.com>
2024-06-15 16:33:44 -0300

    feat: add create user mongo repository

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,6 +1,6 @@
-import { IGetUsersRepository } from "../controllers/get-users/protrocols";
-import { MongoClient } from "../database/mongo";
-import { User } from "../models/user";
+import { IGetUsersRepository } from "../../controllers/get-users/protrocols";
+import { MongoClient } from "../../database/mongo";
+import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
